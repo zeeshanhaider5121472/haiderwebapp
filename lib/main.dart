3ff4ddcbd93e06a3d434e65b27f000718318a755
zeeshanhaider5121472 <shani721287@gmail.com>
2024-10-31 12:35:01 +0500

    share fixed in web but need to comment out andoid emf

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'reusable_widgets/theme/theme_controller.dart';
 import 'routing/app_route_config.dart';
 
 Future<void> main() async {
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: ".env"); 
   // await analytics.logLogin();
 
   // FirebaseAnalytics.instance.logLogin();
@@ -23,6 +23,7 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
+  
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
